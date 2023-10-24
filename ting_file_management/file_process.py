a00dc875750981d3f6f2ebc1085f0578b044183b
Fabricio Floriano <f.florian0@hotmail.com>
2023-10-23 22:40:08 -0300

    Requisito 03?

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,24 @@
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    for index in range(len(instance)):
+        if instance.search(index)['nome_do_arquivo'] == path_file:
+            return None
+        
+    list_set = sorted(set(txt_importer(path_file)))
+
+    file_info = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(list_set),
+        "linhas_do_arquivo": list_set,
+    }
+
+    instance.enqueue(file_info)
+
+    print(file_info)
+
+    return None
 
 
 def remove(instance):
@@ -8,3 +27,4 @@ def remove(instance):
 
 def file_metadata(instance, position):
     """Aqui irá sua implementação"""
+
