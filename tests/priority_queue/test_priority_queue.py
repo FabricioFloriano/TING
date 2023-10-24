15a866be6c173435e583af8759ba7e38fe7ad52f
Fabricio Floriano <f.florian0@hotmail.com>
2023-10-24 00:44:01 -0300

    Requisito 06?

--- tests/priority_queue/test_priority_queue.py
+++ tests/priority_queue/test_priority_queue.py
@@ -1,5 +1,17 @@
-# from ting_file_management.priority_queue import PriorityQueue
+from ting_file_management.priority_queue import PriorityQueue
+import pytest
 
 
 def test_basic_priority_queueing():
-    """Aqui irá sua implementação"""
+    queue = PriorityQueue()
+    queue.enqueue({"qtd_linhas": 4})
+    assert len(queue.high_priority) == 1
+    queue.dequeue()
+    assert len(queue.high_priority) == 0
+    queue.enqueue({"qtd_linhas": 6})
+    assert len(queue.regular_priority) == 1
+    queue.enqueue({"qtd_linhas": 4})
+    queue.enqueue({"qtd_linhas": 3})
+    assert queue.search(1).get("qtd_linhas") == 3
+    with pytest.raises(IndexError, match="Índice Inválido ou Inexistente"):
+        queue.search(-2)
